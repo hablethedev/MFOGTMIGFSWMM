106c51a12c9ffd7e8cda0a1594622c7ab446dbe5
hazel <hazel@DESKTOP-BMADHSH.Home>
2025-03-26 17:37:00 +0000

    Bigger timer, new font method

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/hajimehoshi/ebiten/v2 v2.8.6 // indirect
 	github.com/jezek/xgb v1.1.1 // indirect
 	golang.org/x/image v0.25.0 // indirect
-	golang.org/x/sync v0.8.0 // indirect
+	golang.org/x/sync v0.12.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"image/color"
+	"io/ioutil"
 	"net"
 	"os"
 	"time"
@@ -11,15 +12,15 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"golang.org/x/image/font"
-	"golang.org/x/image/font/inconsolata"
+	"golang.org/x/image/font/opentype"
 )
 
 var (
 	startTime  time.Time
 	running    bool
 	timerValue time.Duration
-	fontFace   font.Face = inconsolata.Bold8x16
-	socketPath string    = "/tmp/timer.sock"
+	fontFace   font.Face
+	socketPath string = "/tmp/timer.sock"
 )
 
 type Game struct{}
@@ -34,11 +35,11 @@ func (g *Game) Update() error {
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.Alpha{0})
 	msg := fmt.Sprintf("%02d:%02d.%03d", int(timerValue.Minutes()), int(timerValue.Seconds())%60, timerValue.Milliseconds()%1000)
-	text.Draw(screen, msg, fontFace, 20, 40, color.White)
+	text.Draw(screen, msg, fontFace, 40, 80, color.White)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 200, 60
+	return 300, 120
 }
 
 func handleConnection(conn net.Conn) {
@@ -81,10 +82,31 @@ func listenUnixSocket() {
 	}
 }
 
+func init() {
+	fontData, err := ioutil.ReadFile("/usr/share/fonts/open-sans/OpenSans-Bold.ttf")
+	if err != nil {
+		panic(err)
+	}
+	
+	tt, err := opentype.Parse(fontData)
+	if err != nil {
+		panic(err)
+	}
+
+	fontFace, err = opentype.NewFace(tt, &opentype.FaceOptions{
+		Size:    32,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+	if err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	go listenUnixSocket()
 
-	ebiten.SetWindowSize(200, 60)
+	ebiten.SetWindowSize(300, 120)
 	ebiten.SetWindowTitle("i am NOT writing that all out as a timer you cant even see")
 	ebiten.SetWindowFloating(true)
 	ebiten.SetWindowDecorated(false)
